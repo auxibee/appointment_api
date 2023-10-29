8eb140985babb5797890fadc2271f7ecc283a59f
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-10-29 19:01:51 -0400

    add auth routes

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const helmet = require('helmet');
 require('express-async-errors');
 
 const healthRoutes = require('./server/health/health.routes');
+const authRoutes = require('./server/auth/auth.routes');
 const { errorHandler, noRouteHandler } = require('./server/shared/middlewares/errorHandler');
 
 const app = express();
 
+// middlewares
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// routes
 app.use('/health', healthRoutes);
+app.use('/auth', authRoutes);
 
+// error middlewares
 app.use(noRouteHandler);
 app.use(errorHandler);
 
