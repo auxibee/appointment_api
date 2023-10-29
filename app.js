91e687606b06d5cac9ffe653298fbf27c7eb4d07
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-10-29 12:26:28 -0400

    add express-async-errors, morgan dependencies

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+require('express-async-errors');
 
 const healthRoutes = require('./server/health/health.routes');
 const { errorHandler, noRouteHandler } = require('./server/shared/middlewares/errorHandler');
 
 const app = express();
 
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
