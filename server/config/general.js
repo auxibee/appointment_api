6921532c24aff5ff7ecb7e0ac88124880304dbdf
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-10-29 19:03:55 -0400

    refactor config.DATABASE_URL to include test

--- server/config/general.js
+++ server/config/general.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const config = {
   PORT: process.env.PORT || 8080,
   DATABASE_URI: process.env.DATABASE_URI,
+  DATABASE_URI_TEST: process.env.DATABASE_URI_TEST,
 };
 
 module.exports = config;
