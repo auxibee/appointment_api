aa3d0d675d5dbce23805c3251acc7a6811d67431
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-10-29 19:05:53 -0400

    refactor config.DATABASE_URL to include test

--- server/shared/connection/connection.js
+++ server/shared/connection/connection.js
@@ -2,7 +2,9 @@
 const { Pool } = require('pg');
 const config = require('../../config/general');
 
-const pool = new Pool({ max: 10, connectionString: config.DATABASE_URI });
+const DATABASE_URI = process.env.NODE_ENV === 'development' ? config.DATABASE_URI : config.DATABASE_URI_TEST;
+
+const pool = new Pool({ max: 10, connectionString: DATABASE_URI });
 
 async function query(statement, values) {
   const client = await pool.connect();
