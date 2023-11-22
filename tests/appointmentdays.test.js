cc6455b304d1814b0aeef0ac9ab10df86a1d0ba4
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-11-21 21:08:01 -0500

    add validation for POST appointmentdays input month limit between 1 and 12 and write test for it

--- tests/appointmentdays.test.js
+++ tests/appointmentdays.test.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+
 const statusCodes = require("../server/shared/statusCodes");
 const { createSuperAdmin } = require("../server/shared/utils/createSuperAdmin");
 
@@ -10,22 +11,28 @@ const { loginUser, createAppointmentDays } = require("./utils");
 
 
 describe('POST /appointmentdays', function(){
+    let login;
     before(async function(){
         // create an admin user
         await createSuperAdmin()
+        login = await loginUser(request, 'fresh@gmail.com','217317auxI*')
     })
 
     it('Creates appointment days', async function (){
       
-  
-      const response = await loginUser(request, 'fresh@gmail.com','217317auxI*')
       
-      const appointmentDays = await createAppointmentDays(request,response.body.token, 2023, 12)
+      const appointmentDays = await createAppointmentDays(request,login.body.token, 2023, 12)
       
       expect(appointmentDays.status).to.eql(201)
       expect(appointmentDays.body.length).to.eql(31);
    
     
+    })
+
+    it('Fails with an out of range month', async function(){
+        const appointmentDays = await createAppointmentDays(request, login.body.token, 2023, 70)
+
+        expect(appointmentDays.status).to.eql(statusCodes.FORBIDDEN)
     })
   
    
