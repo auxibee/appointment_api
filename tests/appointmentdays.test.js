f9d2f8e8c8e65842b9fc578262a77b18459bd922
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-11-21 20:44:03 -0500

    add test for creating appointment days

--- tests/appointmentdays.test.js
+++ tests/appointmentdays.test.js
@@ -7,12 +7,19 @@ const { request, expect } = require("./config");
 const { loginUser, createAppointmentDays } = require("./utils");
 
 
+
+
 describe('POST /appointmentdays', function(){
+    before(async function(){
+        // create an admin user
+        await createSuperAdmin()
+    })
+
     it('Creates appointment days', async function (){
-      // create an admin user
-      const admin = await createSuperAdmin()
+      
   
       const response = await loginUser(request, 'fresh@gmail.com','217317auxI*')
+      
       const appointmentDays = await createAppointmentDays(request,response.body.token, 2023, 12)
       
       expect(appointmentDays.status).to.eql(201)
@@ -21,17 +28,6 @@ describe('POST /appointmentdays', function(){
     
     })
   
-    // it('Fails with an invalid credential', async function(){
-      
-  
-    //   const responseX = await loginUser(request,'wrongemail@gmail.com', 'testpassword')
-    //   const responseY = await loginUser(request,'yaw@gmail.com','wrongpassword')
-  
-    //   expect(responseX.status).to.eql(statusCodes.FORBIDDEN)
-    //   expect(responseX.body.error).to.eql('Wrong username or password')
-  
-    //   expect(responseY.status).to.eql(statusCodes.FORBIDDEN)
-    //   expect(responseY.body.error).to.eql('Wrong username or password')
-    // })
+   
   })
   
