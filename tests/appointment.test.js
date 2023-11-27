1ea3ac6d6c26db38980b1ee6fc92db5835f38a1d
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-11-26 19:15:47 -0500

    add tests for appointment

---
+++ tests/appointment.test.js
@@ -0,0 +1,55 @@
+const config = require("../server/config/general");
+const statusCodes = require("../server/shared/statusCodes");
+
+const { Appointment, AppointmentDay, User } = require("../models");
+const {
+  request,
+  expect,
+  commonHeaders,
+  routes,
+  appointmentDays,
+} = require("./config");
+
+const { resetDb, postRequest } = require("./utils");
+
+const { faker } = require("@faker-js/faker");
+const { generateToken } = require("../server/shared/utils/token");
+
+describe("POST /appointment", function () {
+  let user, appointmentDay, token;
+  before(async () => {
+    await resetDb();
+    user = await User.create({
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    });
+    appointmentDay = await AppointmentDay.create({ day: "2023-12-01" });
+
+    token = await generateToken(user.id);
+  });
+
+  it("creates a new appointment", async function () {
+    const response = await postRequest({
+      url: routes.appointment,
+      token: token,
+      data: { userId: user.id, appointmentDayId: appointmentDay.id },
+    });
+
+    expect(response.status).to.eql(statusCodes.CREATED);
+    expect(response.body.id).to.eql(1);
+  });
+
+  it("fails when appointment has already been created", async () => {
+    //  insert the same userId and AppointmentDayId
+    const response = await postRequest({
+      url: routes.appointment,
+      token: token,
+      data: { userId: user.id, appointmentDayId: appointmentDay.id },
+    });
+
+    expect(response.status).to.eql(statusCodes.FORBIDDEN);
+    expect(response.body.error).to.eql(
+      "Delete appointment before creating another one"
+    );
+  });
+});
