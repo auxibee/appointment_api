77556c9ed9bf39c4dc5b7eea3c24f647b5303e21
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-11-26 19:19:25 -0500

    place before hooks in describe block

--- tests/admin.test.js
+++ tests/admin.test.js
@@ -1,97 +1,106 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const {AppointmentDay} = require('../models')
-const config = require('../server/config/general');
+const { AppointmentDay } = require("../models");
+const config = require("../server/config/general");
 const statusCodes = require("../server/shared/statusCodes");
 const { createSuperAdmin } = require("../server/shared/utils/createSuperAdmin");
 
-
 const { request, expect, routes } = require("./config");
-const { loginUser, createAppointmentDays, updateAppointmentDaySlot, resetDb, getRequest } = require("./utils");
+const {
+  loginUser,
+  createAppointmentDays,
+  updateAppointmentDaySlot,
+  resetDb,
+  getRequest,
+} = require("./utils");
 
-
-  
 let login;
-before(async function(){
+
+describe("POST /appointmentdays", function () {
+  before(async function () {
     // reset database
-    await resetDb()
+    await resetDb();
 
     // create an admin user
-    await createSuperAdmin()
-    login = await loginUser(request, config.ADMIN_EMAIL,config.ADMIN_PASSWORD)
-    
-})
+    await createSuperAdmin();
+    login = await loginUser(request, config.ADMIN_EMAIL, config.ADMIN_PASSWORD);
+  });
 
+  it("Creates appointment days", async function () {
+    const appointmentDays = await createAppointmentDays(
+      request,
+      login.body.token,
+      2023,
+      12
+    );
 
+    expect(appointmentDays.status).to.eql(201);
+    expect(appointmentDays.body.length).to.eql(31);
+  });
 
-describe('POST /appointmentdays', function(){
- 
+  it("Fails with an out of range month", async function () {
+    const appointmentDays = await createAppointmentDays(
+      request,
+      login.body.token,
+      2023,
+      70
+    );
 
-    it('Creates appointment days', async function (){
-      
-      
-      const appointmentDays = await createAppointmentDays(request,login.body.token, 2023, 12)
-      
-      expect(appointmentDays.status).to.eql(201)
-      expect(appointmentDays.body.length).to.eql(31);
-   
-    
-    })
+    expect(appointmentDays.status).to.eql(statusCodes.FORBIDDEN);
+  });
 
-    it('Fails with an out of range month', async function(){
-        const appointmentDays = await createAppointmentDays(request, login.body.token, 2023, 70)
+  it("fails without year input", async function () {
+    const appointmentDays = await createAppointmentDays(
+      request,
+      login.body.token,
+      "",
+      10
+    );
 
-        expect(appointmentDays.status).to.eql(statusCodes.FORBIDDEN)
-    })
+    expect(appointmentDays.status).to.eql(statusCodes.FORBIDDEN);
+  });
 
-    it('fails without year input', async function(){
-        const appointmentDays = await createAppointmentDays(request, login.body.token, '', 10)
-        
-        expect(appointmentDays.status).to.eql(statusCodes.FORBIDDEN)
-        
-    })
+  it("fails without month input", async function () {
+    const appointmentDays = await createAppointmentDays(
+      request,
+      login.body.token,
+      2,
+      ""
+    );
 
-    it('fails without month input', async function(){
-        const appointmentDays = await createAppointmentDays(request, login.body.token, 2, '')
-        
-        expect(appointmentDays.status).to.eql(statusCodes.FORBIDDEN)
-    })
+    expect(appointmentDays.status).to.eql(statusCodes.FORBIDDEN);
+  });
 
-    it('updates avialable slots for appointment', async function(){
-        // const appointmentDays = await createAppointmentDays(request,login.body.token, 2023, 12)
+  it("updates avialable slots for appointment", async function () {
+    // const appointmentDays = await createAppointmentDays(request,login.body.token, 2023, 12)
 
-        const appointmentDay = await updateAppointmentDaySlot(request, login.body.token, 50)
-       
-        expect(appointmentDay.status).to.eql(200)
+    const appointmentDay = await updateAppointmentDaySlot(
+      request,
+      login.body.token,
+      50
+    );
 
-    })
+    expect(appointmentDay.status).to.eql(200);
+  });
+});
 
-   
-  
-   
-  })
- 
+describe("GET /appointmentday/:appointmentDayId", function () {
+  it("returns appointment day status and slots avialable", async () => {
+    const appointmentDay = await AppointmentDay.create({ day: "2023-10-01" });
 
-describe('GET /appointmentday/:appointmentDayId', function (){
+    const url = routes.appointmentDays + `/${appointmentDay.id}`;
 
-    it('returns appointment day status and slots avialable', async () => {
-        const appointmentDay = await AppointmentDay.create({day : '2023-10-01'})
+    const appointmentDays = await getRequest({ url, token: login.body.token });
 
-        const url = routes.appointmentDays + `/${appointmentDay.id}`
-        
-        
-        const appointmentDays = await getRequest({url, token: login.body.token})
-        
-        expect(appointmentDays.status).to.eql(200)
-        expect(appointmentDays.body.slots).to.eql(100)
-        expect(appointmentDays.body.status).to.eql('open')
-        expect(appointmentDays.body.id).to.eql(appointmentDay.id)
-    })
+    expect(appointmentDays.status).to.eql(200);
+    expect(appointmentDays.body.slots).to.eql(100);
+    expect(appointmentDays.body.status).to.eql("open");
+    expect(appointmentDays.body.id).to.eql(appointmentDay.id);
+  });
 
-    it('returns 404 with a non existing apppointment day', async () => {
-        const url = routes.appointmentDays + `/100000`
-        const appointmentDays = await getRequest({url, token: login.body.token})
-        expect(appointmentDays.status).to.eql(404)
-    })
-})
-  
+  it("returns 404 with a non existing apppointment day", async () => {
+    const url = routes.appointmentDays + `/100000`;
+    const appointmentDays = await getRequest({ url, token: login.body.token });
+    expect(appointmentDays.status).to.eql(404);
+  });
+});
--- tests/auth.test.js
+++ tests/auth.test.js
@@ -3,87 +3,102 @@
 const statusCodes = require("../server/shared/statusCodes");
 const { request, expect, commonHeaders, routes } = require("./config");
 const { createUser, loginUser, resetDb } = require("./utils");
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
-before(resetDb)
+describe("POST /auth/signup", function () {
+  before(resetDb);
+  it("creates a new user", async function () {
+    const user = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    const response = await request
+      .post(routes.signUp)
+      .set(commonHeaders)
+      .send({ email: user.email, password: user.password });
 
-describe('POST /auth/signup', function (){
-  it("creates a new user", async function(){
-    const user = {email : faker.internet.email(), password: faker.internet.password()}
-    const response = await request.post(routes.signUp)
-                                  .set(commonHeaders)
-                                  .send({email: user.email, password: user.password})
-                           
     expect(response.status).to.eql(201);
-    expect(response.body).to.deep.equal({id: response.body.id, email: user.email})
+    expect(response.body).to.deep.equal({
+      id: response.body.id,
+      email: user.email,
+    });
+  });
 
-  })
+  it("fails with invalid email", async function () {
+    const response = await request
+      .post(routes.signUp)
+      .set(commonHeaders)
+      .send({
+        email: faker.internet.userName(),
+        password: faker.internet.password(),
+      });
 
-  it("fails with invalid email", async function(){
-    const response = await  request.post(routes.signUp)
-                                   .set(commonHeaders)
-                                   .send({email: faker.internet.userName(), password: faker.internet.password()})
-    
-    expect(response.status).to.eql(statusCodes.FORBIDDEN)
-    
-    expect(response.body.error.length).to.eql(1)
-    expect(response.body.error[0].msg).to.eql('Email should be valid')
-    
-  })
+    expect(response.status).to.eql(statusCodes.FORBIDDEN);
 
-  
+    expect(response.body.error.length).to.eql(1);
+    expect(response.body.error[0].msg).to.eql("Email should be valid");
+  });
 
-  it("fails without a password or password with less than 5 characters", async function(){
-    const response = await request.post(routes.signUp)
-                                  .set(commonHeaders)
-                                  .send({email: faker.internet.email(), password: ''})
-    
-    expect(response.status).to.eql(statusCodes.FORBIDDEN)
-    expect(response.body.error.length).to.eql(2)
-    expect(response.body.error[0].msg).to.eql('Password cannot be empty')
-    expect(response.body.error[1].msg).to.eql('Password Lenght must be atleat 5 characters')
-    
-  })
-})
+  it("fails without a password or password with less than 5 characters", async function () {
+    const response = await request
+      .post(routes.signUp)
+      .set(commonHeaders)
+      .send({ email: faker.internet.email(), password: "" });
 
-describe('POST auth/login', function(){
-  it('Authenticates a user with a valid credential', async function (){
+    expect(response.status).to.eql(statusCodes.FORBIDDEN);
+    expect(response.body.error.length).to.eql(2);
+    expect(response.body.error[0].msg).to.eql("Password cannot be empty");
+    expect(response.body.error[1].msg).to.eql(
+      "Password Lenght must be atleat 5 characters"
+    );
+  });
+});
+
+describe("POST auth/login", function () {
+  it("Authenticates a user with a valid credential", async function () {
     // create a new user
-    const user = {email: faker.internet.email(), password: faker.internet.password()}
-    const newUser = await request.post(routes.signUp)
-                                 .set(commonHeaders)
-                                 .send(user)
+    const user = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    const newUser = await request
+      .post(routes.signUp)
+      .set(commonHeaders)
+      .send(user);
 
-    const response = await request.post(routes.login)
-                                  .send(user)
-    const keys = Object.keys(response.body)
-    expect(response.status).to.eql(200)
-    expect(response.body.email).to.eql(user.email)
-    expect(keys).to.include.members(['id', 'token', 'email']);
-  })
+    const response = await request.post(routes.login).send(user);
+    const keys = Object.keys(response.body);
+    expect(response.status).to.eql(200);
+    expect(response.body.email).to.eql(user.email);
+    expect(keys).to.include.members(["id", "token", "email"]);
+  });
 
-  it('Fails with an invalid credential', async function(){
-    
-    const user = {email: faker.internet.email(), password: faker.internet.password()}
+  it("Fails with an invalid credential", async function () {
+    const user = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
 
     // create a new user
-    const newUser = await request.post(routes.signUp)
-                                 .set(commonHeaders)
-                                 .send(user)
+    const newUser = await request
+      .post(routes.signUp)
+      .set(commonHeaders)
+      .send(user);
 
-    const wrongEmail = await request.post(routes.login)
-                                    .set(commonHeaders)
-                                    .send({email: faker.internet.email(), password: user.password})
-    
-    const wrongPassword = await request.post(routes.login)
-                                    .set(commonHeaders)
-                                    .send({email: user.email, password: faker.internet.password()})
-    
-    expect(wrongEmail.status).to.eql(statusCodes.FORBIDDEN)
-    expect(wrongEmail.body.error).to.eql('Wrong username or password')
+    const wrongEmail = await request
+      .post(routes.login)
+      .set(commonHeaders)
+      .send({ email: faker.internet.email(), password: user.password });
 
-    expect(wrongPassword.status).to.eql(statusCodes.FORBIDDEN)
-    expect(wrongPassword.body.error).to.eql('Wrong username or password')
-  })
-})
+    const wrongPassword = await request
+      .post(routes.login)
+      .set(commonHeaders)
+      .send({ email: user.email, password: faker.internet.password() });
 
+    expect(wrongEmail.status).to.eql(statusCodes.FORBIDDEN);
+    expect(wrongEmail.body.error).to.eql("Wrong username or password");
+
+    expect(wrongPassword.status).to.eql(statusCodes.FORBIDDEN);
+    expect(wrongPassword.body.error).to.eql("Wrong username or password");
+  });
+});
